fd3c8359cc873fd93e77ef58cfe26e9f8d6296f8
pinaruyar <42393801+pinaruyar@users.noreply.github.com>
2025-02-18 11:03:12 +0000

    CCET-526-terraform-initialize (#1)
    * initializing the deploy.sh and terraform structure
    
    * topics were included
    
    * destroy file has been modified
    
    * tidy up
    
    * default region value added
    
    * adding the region check + cahnging the depends_on
    
    * dependency added
    
    * changed topic names

---
+++ infrastructure/modules/confluent-cloud/variables.tf
@@ -0,0 +1,20 @@
+variable "confluent_cloud_environment" {
+  description = "The environment configuration for Confluent Cloud"
+  type = object({
+    name = string
+  })
+}
+variable "confluent_cloud_region" {
+  description = "The region of Confluent Cloud Network"
+  type        = string
+}
+
+variable "confluent_cloud_service_provider" {
+  description = "The cloud provider of Confluent Cloud Network"
+  type        = string
+}
+
+variable "env_display_id_postfix" {
+  description = "A random string we will be appending to resources like environment, api keys, etc. to make them unique"
+  type        = string
+}
