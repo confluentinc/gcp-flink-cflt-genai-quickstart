bda402e0622c932d00fc7bcba5d8703987d76dd8
stephenheg <shegarty@confluent.io>
2025-04-02 20:28:39 +0100

    improve handling on failed builds and deploys without bash v4

--- services/deploy.sh
+++ services/deploy.sh
@@ -63,17 +63,15 @@ fi
 # Lower case the UNIQUE_ID and set service names
 LOWER_UNIQUE_ID=$(echo "$UNIQUE_ID" | tr '[:upper:]' '[:lower:]')
 
-build_failed=0
-deploy_failed=0
-
-declare -A job_status # Associative array to store exit status by PID
-declare -A deploy_status # Associative array to store exit status by PID
-
 AUDIO_TEXT_CONVERTER_SVC_NAME="quickstart-healthcare-ai-audio-text-converter-$LOWER_UNIQUE_ID"
 BUILD_QUERY_SVC_NAME="quickstart-healthcare-ai-build-query-$LOWER_UNIQUE_ID"
 EXECUTE_QUERY_SVC_NAME="quickstart-healthcare-ai-execute-query-$LOWER_UNIQUE_ID"
 SUMMARISE_SVC_NAME="quickstart-healthcare-ai-summarise-$LOWER_UNIQUE_ID"
 WEBSOCKET_SVC_NAME="quickstart-healthcare-ai-websocket-$LOWER_UNIQUE_ID"
+
+
+failed_builds=()
+failed_deploys=()
 
 # Function to build Maven projects
 build_maven_project() {
@@ -82,7 +80,6 @@ build_maven_project() {
     IMAGE_ARCH=$IMAGE_ARCH docker run -v "$service_path":/root/source/ --rm --name build-$(basename "$service_path") maven:3.8.7-openjdk-18-slim sh -c "cd /root/source/ && mvn -T 1C clean install -Dmaven.test.skip -DskipTests -Dmaven.javadoc.skip=true"
     if [ $? -ne 0 ]; then
         echo "[-] Failed to build $service_path"
-        build_failed=1
         return 1
     fi
     echo "[+] $service_path built successfully"
@@ -95,8 +92,6 @@ build_node_project() {
     IMAGE_ARCH=$IMAGE_ARCH docker run -v "$service_path":/root/source/ --rm --name build-frontend node:current-alpine3.20 sh -c "cd /root/source/frontend && npm i && npm run build"
     if [ $? -ne 0 ]; then
         echo "[-] Failed to build $service_path/frontend"
-        build_failed=1
-        echo "Build failed with status: $build_failed"
         return 1
     fi
     echo "[+] $service_path/frontend built successfully"
@@ -111,44 +106,35 @@ deploy_gcloud() {
     IMAGE_ARCH=$IMAGE_ARCH docker run -v "$CONFIG_FOLDER":/root/.config/ -v "$service_path":/root/source/ --rm --name quickstart-deploy-$svc_name gcr.io/google.com/cloudsdktool/google-cloud-cli:stable gcloud run deploy "$svc_name" --no-cpu-throttling --source "/root/source/" --region "$GCP_REGION" --allow-unauthenticated --cpu 2 --memory 1Gi --project "$GCP_PROJECT_ID" --min-instances 1 --set-env-vars "$extra_vars"
     if [ $? -ne 0 ]; then
         echo "[-] Failed to deploy $svc_name"
-        deploy_failed=1
-        echo "Deploy failed with status: $deploy_failed"
         return 1
     fi
     echo "[+] $svc_name deployed successfully"
 }
 
 # Parallel build process for Maven and Node projects
-build_maven_project "$SCRIPT_FOLDER/audio-text-converter" & job_status[$!]=$? # Capture PID and placeholder exit status
-build_maven_project "$SCRIPT_FOLDER/build-query" & job_status[$!]=$? # Capture PID and placeholder exit status
-build_maven_project "$SCRIPT_FOLDER/execute_query" & job_status[$!]=$? # Capture PID and placeholder exit status
-build_maven_project "$SCRIPT_FOLDER/summarize" & job_status[$!]=$? # Capture PID and placeholder exit status
-build_node_project "$SCRIPT_FOLDER/websocket" & job_status[$!]=$? # Capture PID and placeholder exit status
-wait
+build_maven_project "$SCRIPT_FOLDER/audio-text-converter" & job_ids+=($!) services+=("audio-text-converter")
+build_maven_project "$SCRIPT_FOLDER/build-query" & job_ids+=($!) services+=("build-query")
+build_maven_project "$SCRIPT_FOLDER/execute_query" & job_ids+=($!) services+=("execute_query")
+build_maven_project "$SCRIPT_FOLDER/summarize" & job_ids+=($!) services+=("summarize")
+build_node_project "$SCRIPT_FOLDER/websocket" & job_ids+=($!) services+=("websocket")
 
-# Waiting on each job and capturing its exit status
-for pid in "${!job_status[@]}"; do
-    wait "$pid"
-    job_status[$pid]=$? # Update the placeholder with the actual exit status
-done
-
-# Check the exit status of all jobs
-for status in "${job_status[@]}"; do
-    if [ "$status" -ne 0 ]; then
-        build_failed=1
-        break
+for i in "${!job_ids[@]}"; do
+    if ! wait "${job_ids[$i]}"; then
+        # If wait command returns a non-zero status, the job failed
+        failed_builds+=("${services[$i]}")
     fi
 done
 
-echo "build status: " $build_failed
-
-# Check if any build failed
-if [ $build_failed -ne 0 ]; then
-    echo "[-] One or more builds failed. Cancelling deployment and starting cleanup....."
+if [ "${#failed_builds[@]}" -ne 0 ]; then
+    echo "[-] Some builds failed:"
+    for service in "${failed_builds[@]}"; do
+        echo " - $service"
+    done
+    echo "[-] Starting cleanup of quickstart setup"
     "$SCRIPT_FOLDER/../destroy.sh"
     exit 1
 else
-    echo "[+] All builds succeeded. Proceeding with deployment."
+    echo "[+] All builds completed successfully"
 fi
 
 # Set environment variable string for gcloud deployments
@@ -159,32 +145,29 @@ execute_query_env_vars="$common_env_vars,TOPIC_IN=generated_sql,TOPIC_OUT=sql_re
 summarise_env_vars="$common_env_vars,TOPIC_IN=sql_results,TOPIC_OUT=summarised_results"
 
 # Parallel deployment process
-deploy_gcloud "$AUDIO_TEXT_CONVERTER_SVC_NAME" "$SCRIPT_FOLDER/audio-text-converter" "$audio_text_converter_env_vars" & deploy_status[$!]=$?
-deploy_gcloud "$BUILD_QUERY_SVC_NAME" "$SCRIPT_FOLDER/build-query" "$build_query_env_vars" & deploy_status[$!]=$?
-deploy_gcloud "$EXECUTE_QUERY_SVC_NAME" "$SCRIPT_FOLDER/execute_query" "$execute_query_env_vars" & deploy_status[$!]=$?
-deploy_gcloud "$SUMMARISE_SVC_NAME" "$SCRIPT_FOLDER/summarize" "$summarise_env_vars" & deploy_status[$!]=$?
-deploy_gcloud "$WEBSOCKET_SVC_NAME" "$SCRIPT_FOLDER/websocket" "$common_env_vars" & deploy_status[$!]=$?
-wait
+deploy_gcloud "$AUDIO_TEXT_CONVERTER_SVC_NAME" "$SCRIPT_FOLDER/audio-text-converter" "$audio_text_converter_env_vars" & job_ids+=($!)
+                                                                                                                        services+=("audio-text-converter")
+deploy_gcloud "$BUILD_QUERY_SVC_NAME" "$SCRIPT_FOLDER/build-query" "$build_query_env_vars" & job_ids+=($!) services+=("build-query")
+deploy_gcloud "$EXECUTE_QUERY_SVC_NAME" "$SCRIPT_FOLDER/execute_query" "$execute_query_env_vars" & job_ids+=($!) services+=("execute_query")
+deploy_gcloud "$SUMMARISE_SVC_NAME" "$SCRIPT_FOLDER/summarize" "$summarise_env_vars" & job_ids+=($!) services+=("summarize")
+deploy_gcloud "$WEBSOCKET_SVC_NAME" "$SCRIPT_FOLDER/websocket" "$common_env_vars" & job_ids+=($!) services+=("websocket")
 
-# Waiting on each deploy job and capturing its exit status
-for pid in "${!deploy_status[@]}"; do
-    wait "$pid"
-    deploy_status[$pid]=$? # Update the placeholder with the actual exit status
-done
-
-# Check the exit status of all jobs
-for status in "${deploy_status[@]}"; do
-    if [ "$status" -ne 0 ]; then
-        deploy_failed=1
-        break
+for i in "${!job_ids[@]}"; do
+    if ! wait "${job_ids[$i]}"; then
+        # If wait command returns a non-zero status, the job failed
+        failed_deploys+=("${services[$i]}")
     fi
 done
 
 # After deployment process, check if any deployments failed
-if [ $deploy_failed -ne 0 ]; then
-    echo "[-] One or more deployments failed. Initiating cleanup of quickstart...."
+if [ "${#failed_deploys[@]}" -ne 0 ]; then
+    echo "[-] Some services failed to deploy:"
+    for service in "${failed_deploys[@]}"; do
+        echo " - $service"
+    done
+    echo "[-] Starting cleanup of quickstart setup"
     "$SCRIPT_FOLDER/../destroy.sh"
     exit 1
 else
-    echo "[+] All deployments completed successfully."
+    echo "[+] All deployments completed successfully"
 fi
