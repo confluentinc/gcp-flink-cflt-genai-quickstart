383ee9ec0d731c345dead14c3271daa93ec68085
stephenheg <shegarty@confluent.io>
2025-04-07 08:55:21 +0100

    WIP flink key schemas update

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -62,7 +62,6 @@ variable "env_display_id_postfix" {
 variable "gcloud_project" {
   description = "Google Cloud Project ID"
   type        = string
-  default     = "csid-281116"
 }
 
 variable "bigquery_db" {
